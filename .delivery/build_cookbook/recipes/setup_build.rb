dc6e4c661d2d2ab40448b4b00cddd645662d5c91
Jp Robinson <dreamnite@penguinguru.com>
2017-05-07 16:42:47 -0400

    Setup build recipe, start unit test

--- .delivery/build_cookbook/recipes/setup_build.rb
+++ .delivery/build_cookbook/recipes/setup_build.rb
@@ -5,3 +5,30 @@
 # Copyright:: 2017, Jp Robinson, All Rights Reserved.
 
 # Steps to set up a build of the correct apache version
+buid_config = ab_load_conifg("#{workflow_workspace_repo}/#{node[apache_build][config_file]}") # Load and parse the config file
+
+build_config['required_build_packages'].each do |cur_package|
+    package cur_package # Install packages required to make the build run
+end
+
+src_dir="#{workflow_workspace_repo}/httpd" # Root directory for the source to go into on the build node
+
+# Check out source code from git
+git src_dir do
+repository build_config['apache_source']
+revision build_config['apache_version']
+end
+
+git "#{src_dir}/srclib/apr" do
+repository build_config['apr_source']
+revision build_config['apr_version']
+end
+git "#{src_dir}/srclib/apr-util" do
+repository build_config['apr_utils_source']
+revision build_config['apr_utils_version']
+end
+
+bash 'Building autoconf script' do
+  code "#{src_dir}/buildconf"
+end
+
--- .delivery/build_cookbook/recipes/unit.rb
+++ .delivery/build_cookbook/recipes/unit.rb
@@ -3,3 +3,7 @@
 # Recipe:: unit
 #
 # Copyright:: 2017, Jp Robinson, All Rights Reserved.
+
+# Our unit for approval is that the code must compile. Failed compliation, no reason to move on.
+include_recipe 'build_cookbook::setup_build' # Set up the workspace
+include_recipe 'build_cookbook::perform_build' # Compile
