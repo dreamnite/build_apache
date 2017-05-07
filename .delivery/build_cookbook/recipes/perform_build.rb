49253c6fa6fdb7549c11ef651699f76947c1f5eb
Jp Robinson <dreamnite@penguinguru.com>
2017-05-07 19:41:17 -0400

    Add build steps

--- .delivery/build_cookbook/recipes/perform_build.rb
+++ .delivery/build_cookbook/recipes/perform_build.rb
@@ -4,4 +4,19 @@
 #
 # Copyright:: 2017, Jp Robinson, All Rights Reserved.
 
+include_recipe 'build_cookbook::setup_build' ## Just to be sure we don't accidently forget to include the setup phase
 ## Steps to actually build/compile apache
+build_config = ab_load_config(node['apache_build']['config_file']) # Load and parse the config file
+
+src_dir = "#{workflow_workspace_repo}/httpd" # Root source directory
+
+bash "Running configure with options: #{build_config['configure_options'].join(', ')}" do
+  code "#{src_dir}/configure #{build_config['configure_options'].map { |opt| "--#{opt}"}.join (' ')}"
+  cwd src_dir
+end
+
+bash 'Running Make' do
+  code "make"
+  cwd src_dir
+end
+
