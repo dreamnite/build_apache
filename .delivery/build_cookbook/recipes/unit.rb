5b8804a1d39b8f0329b5fed662830f840ff878c8
Jp Robinson <dreamnite@penguinguru.com>
2017-05-09 01:23:13 -0400

    Make unit into provision for testing

--- .delivery/build_cookbook/recipes/unit.rb
+++ .delivery/build_cookbook/recipes/unit.rb
@@ -5,6 +5,6 @@
 # Copyright:: 2017, Jp Robinson, All Rights Reserved.
 
 # Our unit for approval is that the code must compile. Failed compliation, no reason to move on.
-include_recipe 'build_cookbook::setup_build' # Set up the workspace
-include_recipe 'build_cookbook::perform_build' # Compile
-
+# include_recipe 'build_cookbook::setup_build' # Set up the workspace
+# include_recipe 'build_cookbook::perform_build' # Compile
+include_recipe 'build_cookbook::provision'
