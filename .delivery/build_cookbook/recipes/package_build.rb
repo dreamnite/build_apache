d190d8a9e66782b8150ad9c39e7bb6d399ea2439
Jp Robinson <dreamnite@penguinguru.com>
2017-05-09 00:15:19 -0400

    Make ssh key a sensitive resource

--- .delivery/build_cookbook/recipes/package_build.rb
+++ .delivery/build_cookbook/recipes/package_build.rb
@@ -37,6 +37,7 @@ with_server_config do # Chef server context, so we can get the databag.
     file "#{workflow_workspace_repo}/ssh_key" do
       content publish_info['key']
       mode '0700'
+      sensitive true
     end
 
     sudo = 'sudo' if publish_info['sudo']
--- .delivery/build_cookbook/recipes/unit.rb
+++ .delivery/build_cookbook/recipes/unit.rb
@@ -7,4 +7,4 @@
 # Our unit for approval is that the code must compile. Failed compliation, no reason to move on.
 include_recipe 'build_cookbook::setup_build' # Set up the workspace
 include_recipe 'build_cookbook::perform_build' # Compile
-include_recipe 'build_cookbook::package_build' # for temp debugging
+
