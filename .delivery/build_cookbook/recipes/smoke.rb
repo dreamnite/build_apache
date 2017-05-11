436fbc0919cf1eacedeff087d88857b30796e392
Jp Robinson <dreamnite@penguinguru.com>
2017-05-11 02:36:30 -0400

    Add Smoke phase

--- .delivery/build_cookbook/recipes/smoke.rb
+++ .delivery/build_cookbook/recipes/smoke.rb
@@ -3,3 +3,22 @@
 # Recipe:: smoke
 #
 # Copyright:: 2017, Jp Robinson, All Rights Reserved.
+
+build_root = "#{workflow_workspace_repo}/build"
+
+template '#{workflow_workspace_repo}/min_httpd_conf.conf' do
+  source 'min_httpd_conf.erb'
+  variables(server_root: build_root)
+end
+
+execute 'Start apache locally' do
+  command "#{build_root}/bin/httpd -k start -f #{workflow_workspace_repo}/min_httpd_conf.conf"
+end
+
+http_request 'test_request' do
+  url 'http://localhost:12345/'
+end
+
+execute 'Stop apache locally' do
+  command "#{build_root}/bin/httpd -k stop -f #{workflow_workspace_repo}/min_httpd_conf.conf"
+end
